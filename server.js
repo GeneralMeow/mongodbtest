7003f031782f515d7178a24a9e7c6651bf87a51a
generalmeow <rcbrown@gmail.com>
2016-11-17 14:57:41 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+const ObjectID = mongodb.ObjectID;
+
+const CONTACTS_COLLECTION = "contacts";
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+//create a database variable outside of the database connection callback to reuse the connection pool in your app
+var db;
+
+//connect to the database before starting the application server
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+//save database object from the callback for reuse
+db = database;
+console.log("Database connection ready");
+
+//initialize the app
+const server = app.listen(process.env.PORT || 8080, function (){
+  const port = server.address().port;
+  console.log("App now running on port", port);
+  });
+});
+
+//contacts api routes below
+
+//generic error handler used by all endpoints
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/* "/contacts"
+* GET: finds all contacts
+* POST: creates a new contact
+*/
+
+//GET: finds all contacts
+app.get("/contacts", function(req, res) {
+});
+
+//POST: creates a new contact
+app.post("/contacts", function(req, res) {
+  const newContact = req.body;
+  newContact.createDate = new Date();
+
+  if(!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if(err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/* "/contacts/:id"
+* GET:find all contacts by id
+*PUT: update contact by id
+*DELETE: deletes contact by id
+*/
+
+app.get("/contacts/:id", function(req, res) {
+
+});
+
+app.put("/contacts/:id", function(req, res) {
+
+});
+
+app.delete("/contacts/:id", function(req, res) {
+
+});
